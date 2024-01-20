a04a1142e7f59d9a1cde383a10c16e74d55742cb
Rolland306 <rollandmuzeya@gmail.com>
2024-01-19 23:08:34 -0500

    testing cards functionality

--- telp-dashboard-v1/app/dashboard/page.tsx
+++ telp-dashboard-v1/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { Card } from '@/app/ui/dashboard/cards';
 import { lusitana } from '@/app/ui/fonts';
-//import { fetchCardData } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 //import { Suspense } from 'react';
 // import {
 //   RevenueChartSkeleton,
@@ -8,18 +8,18 @@ import { lusitana } from '@/app/ui/fonts';
 // } from '@/app/ui/skeletons';
 
 export default async function Page() {
-  //   const {
-  //     numberOfIncidents,
-  //     numberOfTeachers,
-  //     numberOfComments,
-  //     numberOfPendingComments,
-  //   } = await fetchCardData();
+  const {
+    numberOfIncidents,
+    numberOfTeachers,
+    numberOfComments,
+    numberOfPendingComments,
+  } = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      {/*<div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         {
           //the type relates to which icon is selected
           //from iconMap in /ui/dashboard/cards.tsx
@@ -50,7 +50,7 @@ export default async function Page() {
             type="pending"
           />
         }
-      </div>*/}
+      </div>
     </main>
   );
 }
