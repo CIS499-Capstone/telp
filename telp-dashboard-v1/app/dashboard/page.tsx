b1a76f98d8ba92b0013608ff18853c446a6b8997
Rolland306 <rollandmuzeya@gmail.com>
2024-01-19 21:49:05 -0500

    had to delete the revenueskeleton and suspense stuff at the bottom of the cards

--- telp-dashboard-v1/app/dashboard/page.tsx
+++ telp-dashboard-v1/app/dashboard/page.tsx
@@ -1,21 +1,19 @@
 import { Card } from '@/app/ui/dashboard/cards';
-//import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data';
-import { Suspense } from 'react';
-import {
-  RevenueChartSkeleton,
-  LatestInvoicesSkeleton,
-} from '@/app/ui/skeletons';
+//import { fetchCardData } from '@/app/lib/data';
+//import { Suspense } from 'react';
+// import {
+//   RevenueChartSkeleton,
+//   LatestInvoicesSkeleton,
+// } from '@/app/ui/skeletons';
 
 export default async function Page() {
-  const {
-    numberOfIncidents,
-    numberOfTeachers,
-    numberOfComments,
-    numberOfPendingComments,
-  } = await fetchCardData();
+  //   const {
+  //     numberOfIncidents,
+  //     numberOfTeachers,
+  //     numberOfComments,
+  //     numberOfPendingComments,
+  //   } = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -53,14 +51,6 @@ export default async function Page() {
           />
         }
       </div>*/}
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense fallback={<RevenueChartSkeleton />}>
-          {/*<RevenueChart />*/}
-        </Suspense>
-        <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices />
-        </Suspense>
-      </div>
     </main>
   );
 }
