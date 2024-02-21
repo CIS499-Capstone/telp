2be7e0a45f285e48c923088f11fdad778e6f9c80
josephclark293 <josephclark293@gmail.com>
2024-02-21 16:43:55 -0500

    add api endpoint

---
+++ telp-dashboard-v1/app/api/esp32/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+const accountSid = 'AC99cb6ddfd6009b2a7e3de2b43fc5a7ed';
+const authToken = '5b624cb19375ac820f58416a53fcabf2';
+const client = require('twilio')(accountSid, authToken);
+// To handle a GET request to /api
+export async function GET(request) {
+  // Do whatever you want
+  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+}
+
+// To handle a POST request to /api
+export async function POST(request) {
+  // Do whatever you want
+
+
+  const dt = new Date().toTimeString();
+  console.log(dt);
+  client.messages
+    .create({
+      body: 'ALERT! Emergency in Mr. Jones CLASSROOM @' + dt,
+      messagingServiceSid: 'MGdd0541aa1a1002062aeef44512e39993',
+      to: '+18033894070'
+    })
+    .then(message => console.log(message.sid));
+  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+}
