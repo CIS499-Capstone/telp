e39eab6fdb69e9deefa665cc52005080c1995a5a
josephclark293 <josephclark293@gmail.com>
2024-02-26 21:05:30 -0500

    pull id out of JSON in API

--- telp-dashboard-v1/app/api/esp32/route.js
+++ telp-dashboard-v1/app/api/esp32/route.js
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
-const accountSid = '';
-const authToken = '';
+import { accountSid, authToken } from "./twilioKeys";
 const client = require('twilio')(accountSid, authToken);
 // To handle a GET request to /api
 export async function GET(request) {
@@ -12,15 +11,16 @@ export async function GET(request) {
 export async function POST(request) {
   // Do whatever you want
 
-
+  const data = await request.json();
+  console.log(data)
   const dt = new Date().toTimeString();
   console.log(dt);
-  client.messages
-    .create({
-      body: 'ALERT! Emergency in Mr. Jones CLASSROOM @' + dt,
-      messagingServiceSid: 'MGdd0541aa1a1002062aeef44512e39993',
-      to: '+18033894070'
-    })
-    .then(message => console.log(message.sid));
-  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+  // client.messages
+  //   .create({
+  //     body: 'ALERT! Emergency in Mr. Jones CLASSROOM @' + dt,
+  //     messagingServiceSid: 'MGdd0541aa1a1002062aeef44512e39993',
+  //     to: '+18033894070'
+  //   })
+  //   .then(message => console.log(message.sid));
+  return NextResponse.json({ message: data.id}, { status: 200 });
 }
