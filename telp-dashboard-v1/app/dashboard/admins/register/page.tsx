0525aaf5b2ef7f1489374da9419da7939f613843
Joshua <123go12.joshua.hogan.2000@gmail.com>
2024-02-28 13:37:12 -0500

    Conditional Rendering

--- telp-dashboard-v1/app/dashboard/admins/register/page.tsx
+++ telp-dashboard-v1/app/dashboard/admins/register/page.tsx
@@ -1,25 +1,35 @@
 import Form from '@/app/ui/admins/register-form';
 import Breadcrumbs from '@/app/ui/breadcrumbs';
 import { Metadata } from 'next';
+import { getTeacher } from '@/app/dashboard/page';
  
 export const metadata: Metadata = {
   title: 'Register Admin',
 };
 
 export default async function Page() {
+  const user = await getTeacher();
    return (
-    <main>
-      <Breadcrumbs
-        breadcrumbs={[
-          { label: 'Admins', href: '/dashboard/admins' },
-          {
-            label: 'Register Admin',
-            href: '/dashboard/admins/register',
-            active: true,
-          },
-        ]}
-      />
-      <Form/>
-    </main>
+    user[0].role === 'teacher' ? (
+      <div className="w-full">
+        <div className="w-full">
+          <h1>Its a Teacher Dawg. They cant view admin level stuff</h1>
+        </div>
+      </div>
+    ) : (
+      <main>
+        <Breadcrumbs
+          breadcrumbs={[
+            { label: 'Admins', href: '/dashboard/admins' },
+            {
+              label: 'Register Admin',
+              href: '/dashboard/admins/register',
+              active: true,
+            },
+          ]}
+        />
+        <Form/>
+      </main>
+    )
   );
 }
