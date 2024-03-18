29a456d615bb0c917e257d4a38d63dfc20ec2740
josephclark293 <josephclark293@gmail.com>
2024-03-18 13:01:02 -0400

    esp32

--- telp-dashboard-v1/app/api/esp32/route.js
+++ telp-dashboard-v1/app/api/esp32/route.js
@@ -15,13 +15,13 @@ export async function POST(request) {
   const data = await request.json();
   const emp_name = await fetchEmpData(data.id);
   const dt = new Date().toTimeString();
-  // client.messages
-  //   .create({
-  //     body: 'ALERT! Emergency with ' + emp_name['name'] + " in " + emp_name['location'] + ' @ ' + dt,
-  //     messagingServiceSid: 'MGdd0541aa1a1002062aeef44512e39993',
-  //     to: '+18033894070'
-  //   })
-  //   .then(message => console.log(message.sid));
+  client.messages
+    .create({
+      body: 'ALERT! Emergency with ' + emp_name['name'] + " in " + emp_name['location'] + ' @ ' + dt,
+      messagingServiceSid: 'MGdd0541aa1a1002062aeef44512e39993',
+      to: '+18033894070'
+    })
+    .then(message => console.log(message.sid));
   await addIncident(data.id);
   return NextResponse.json({ message: emp_name}, { status: 200 });
 }
