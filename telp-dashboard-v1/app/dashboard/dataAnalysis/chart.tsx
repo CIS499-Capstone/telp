f60c12d25c1fbd7bf6718134278361fea63510d5
josephclark293 <josephclark293@gmail.com>
2024-03-19 23:03:57 -0400

    make polar area

--- telp-dashboard-v1/app/dashboard/dataAnalysis/chart.tsx
+++ telp-dashboard-v1/app/dashboard/dataAnalysis/chart.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
+import { ChartType } from 'chart.js';
 
 const ChartComponent: React.FC = () => {
   const chartRef = useRef<HTMLCanvasElement | null>(null);
@@ -18,39 +19,30 @@ const ChartComponent: React.FC = () => {
     }
 
     // Create a new chart instance
+    
+
     chartInstanceRef.current = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+      type: 'polarArea' as ChartType,
+      data: {labels: [
+            'Red',
+            'Green',
+            'Yellow',
+            'Grey',
+            'Blue'
           ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
+          datasets: [{
+            label: 'My First Dataset',
+            data: [11, 16, 7, 3, 14],
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(75, 192, 192)',
+              'rgb(255, 205, 86)',
+              'rgb(201, 203, 207)',
+              'rgb(54, 162, 235)'
+            ]
+         }]
       },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
+      options:{}
     });
 
     // Cleanup function to destroy the chart instance on unmount
--- telp-dashboard-v1/app/dashboard/dataAnalysis/page.tsx
+++ telp-dashboard-v1/app/dashboard/dataAnalysis/page.tsx
@@ -3,7 +3,7 @@ import ChartComponent from "./chart";
 export default async function Page(){
   
   return(
-    <div  className="mt-0 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+    <div  className="mt-0 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8" style={{ width: '75%', height: '75%' }}>
       <h1>My Next.js Chart</h1>
       <ChartComponent />
     </div>
