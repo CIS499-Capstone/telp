3199a44177349cc532cabafbab875bbc4c465287
josephclark293 <josephclark293@gmail.com>
2024-03-25 17:32:19 -0400

    dropdown with student names

--- telp-dashboard-v1/app/dashboard/dataAnalysis/chart.tsx
+++ telp-dashboard-v1/app/dashboard/dataAnalysis/chart.tsx
@@ -1,4 +1,4 @@
-import { fetchCardData } from "@/app/lib/data";
+import { fetchCardData, fetchStudents } from "@/app/lib/data";
 import { unstable_noStore as noStore } from 'next/cache';
 import { Card } from "@/app/ui/dashboard/cards";
 import AreaChart from "./AreaChartPlot";
@@ -20,14 +20,14 @@ export default async function Charts(
     labels: timeData[0].map(String),
     datasets: [{
       label: "Incident Counts over Time",
-      data: timeData[1].map(value => typeof value === 'string'  ? parseInt(value) : value),
+      data: timeData[1].map(value => typeof value === 'string' ? parseInt(value) : value),
       fill: false,
       borderColor: 'rgb(75, 192, 192)',
       tension: 0.1,
       xAxisID: "Incident"
     }]
   }
-  
+
   const pieData = await fetchIncidentsByTeacher();
 
   const chartData =
@@ -49,34 +49,52 @@ export default async function Charts(
       borderWidth: 1
     }]
   }
+  const students = await fetchStudents();
   return (
-    <div className="p-4">
-    {/* Section for cards */}
-    <section className="mb-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-      <Card title="Total Teachers" value={numberOfTeachers} type="teachers" />
-      <Card title="Total Incidents" value={numberOfIncidents} type="incidents" />
-      <Card title="Total Comments" value={numberOfComments} type="comments" />
-      <Card title="Pending Comments" value={numberOfPendingComments} type="pending" />
-    </section>
+    <div className="p-4 w-full">
+      {/* Section for cards */}
+      <section className="mb-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Total Teachers" value={numberOfTeachers} type="teachers" />
+        <Card title="Total Incidents" value={numberOfIncidents} type="incidents" />
+        <Card title="Total Comments" value={numberOfComments} type="comments" />
+        <Card title="Pending Comments" value={numberOfPendingComments} type="pending" />
+      </section>
 
-    {/* Section for charts */}
-    <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
-      {/* Pie chart */}
-      <div className="rounded-lg border bg-white p-4 lg:col-span-2">
-        <ChartComponent data={chartData} />
+      {/* Section for charts */}
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
+        {/* Pie chart */}
+        <div className="rounded-lg border bg-white p-4 lg:col-span-2">
+          <ChartComponent data={chartData} />
+        </div>
+
+        {/* Area chart */}
+        <div className="rounded-lg border bg-white p-4 lg:col-span-2">
+          <AreaChart data={lineData} />
+        </div>
+
+        {/* Bar chart */}
+        <div className="rounded-lg border bg-white p-4 lg:col-span-4">
+          <BarChart data={barData} />
+        </div>
       </div>
 
-      {/* Area chart */}
-      <div className="rounded-lg border bg-white p-4 lg:col-span-2">
-        <AreaChart data={lineData} />
-      </div>
-
-      {/* Bar chart */}
-      <div className="rounded-lg border bg-white p-4 lg:col-span-4">
-        <BarChart data={barData} />
+      {/* Dropdown */}
+      <div className="mt-4">
+        <select
+          id="student_id"
+          name="student_id"
+          className="block w-full rounded-md border border-gray-200 px-3 py-2 text-sm placeholder-gray-500 outline-none"
+        >
+           <option disabled selected>Select Option</option>
+          {students.map((student) => (
+            <option key={student.student_id} value={student.student_id}>
+              {`${student.student_id} - ${student.name}`}
+            </option>
+          ))}
+        </select>
       </div>
     </div>
-  </div>
+
   );
 };
 
